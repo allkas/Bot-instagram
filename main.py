edaef560bcb6fcb7c0e51171b69209d8ff64afc2
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-09-27 13:36:39 +0300

    first(auth)

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from auth_data import username, password
+import time
+import random
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# def login(username, password):
+#     browser = webdriver.Chrome('./chromedriver/chromedriver')
+#     try:
+#         browser.get('https://instagram.com')
+#         time.sleep(random.randrange(3, 5))
+#
+#         username_input = browser.find_element_by_name('username')
+#         username_input.clear()
+#         username_input.send_keys(username)
+#
+#         time.sleep(2)
+#
+#         password_input = browser.find_element_by_name('password')
+#         password_input.clear()
+#         password_input.send_keys(password)
+#
+#         password_input.send_keys(Keys.ENTER)
+#         time.sleep(10)
+#
+#         browser.close()
+#         browser.quit()
+#     except Exception as ex:
+#         print(ex)
+#         browser.close()
+#         browser.quit()
+#
+#
+# login(username, password)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def hash_search(username, password, hashtag):
